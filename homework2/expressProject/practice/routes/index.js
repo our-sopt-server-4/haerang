f48c1020ab19bf727df5bfe55755427c633e4966
haerang <gofkdvjvl@naver.com>
2020-04-25 17:52:05 +0900

    homework2 done

--- homework2/expressProject/practice/routes/index.js
+++ homework2/expressProject/practice/routes/index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
-const index = require("./api");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
+router.use("/api", require("./api"));
 router.use("/blog", require("./blog"));
 
 module.exports = router;
