cb87c0ec4dff01a89098bbd34f27f17b2064e40c
haerang <gofkdvjvl@naver.com>
2020-05-30 12:33:50 +0900

    level 3

--- homework4/practice/models/posts.js
+++ homework4/practice/models/posts.js
@@ -1,24 +1,61 @@
-module.exports = [
-  {
-    id: "1",
-    author: "박경선",
-    title: "qwerty",
-    contents: "첫 게시물 포스팅",
-    date:'2020-01-01'
+const pool = require("../modules/pool");
+let moment = require("moment");
+const table = "post";
+
+const post = {
+  readAllPost: async () => {
+    const query = `SELECT * FROM ${table}`;
+    try {
+      const result = await pool.queryParam(query);
+      return result;
+    } catch (err) {
+      console.log("readAllPost ERROR : ", err);
+      throw err;
+    }
   },
-  {
-    id:"2",
-	author:"최해랑",
-	title:"깡",
-	contents:"화려한 조명이 나를 감싸네",
-    date:"2020-05-29"
-	
-    },
-  {
-    id: "3",
-    author: "최정균",
-    title: "password",
-    contents: "게시글 3개야??",
-    date:'2020-05-29'
-  },    
-];
+  getPost: async (id) => {
+    const query = `SELECT * FROM ${table} WHERE postIdx = ${id}`;
+    try {
+      const result = await pool.queryParam(query);
+      return result;
+    } catch (err) {
+      console.log("readPost ERROR : ", err);
+      throw err;
+    }
+  },
+  writePost: async (author, title, content) => {
+    const createdAt = moment().format("YYYY년 MM월 DD일");
+    const fields = "author, title, content, createdAt";
+    const questions = `?, ?, ?, "${createdAt}"`;
+    const values = [author, title, content];
+    const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
+
+    try {
+      const result = await pool.queryParamArr(query, values);
+      const insertId = result.insertId;
+      return insertId;
+    } catch (err) {
+      console.log("writePost ERROR : ", err);
+      throw err;
+    }
+  },
+  updatePost: async (id, title, content) => {
+    const query = `UPDATE ${table} SET title = "${title}", content = "${content}" WHERE postIdx="${id}"`;
+    try {
+      await pool.queryParam(query);
+    } catch (err) {
+      console.log("updatePost ERROR : ", err);
+      throw err;
+    }
+  },
+  deletePost: async (id) => {
+    const query = `DELETE FROM ${table} where postIdx="${id}"`;
+    try {
+      await pool.queryParam(query);
+    } catch (err) {
+      console.log("deletePost ERROR : ", err);
+      throw err;
+    }
+  },
+};
+module.exports = post;
